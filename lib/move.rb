030ae534734c6586ed798940ab94fa6a9ca5fcfa
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-22 00:13:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,4 +10,11 @@ def input_to_index(input)
 end
 def move(board,index,current_player="X")
   board[index]=current_player
+
+#def input_to_index(index)
+  #index=input.to_i - 1
+#end
+#def move(board,index,"X")
+#  array[index]=value
+#>>>>>>> ee8d769ab98734d36b9d9781574d0db773ab9d74
 end
